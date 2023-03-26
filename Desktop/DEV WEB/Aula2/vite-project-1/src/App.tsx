e476a343985d60d6859dbd8e3a37fd9ccc2c369b
Fernandopassos29 <113645797+Fernandopassos29@users.noreply.github.com>
2023-03-26 18:52:12 -0300

    Aula 02

---
+++ Desktop/DEV WEB/Aula2/vite-project-1/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <p>Teste 02</p>
+  )
+}
+
+export default App
